ca8ef8619c80d360d8ef040cb7645934c5a75204
deltalite <dmvanlee@edu.uwaterloo.ca>
2020-01-02 23:12:33 -0500

    scala bin search

---
+++ Scala/binary_search.scala
@@ -0,0 +1,26 @@
+object BinSearch
+{ 
+    // tests to come
+    def main(args: Array[String]) {
+    { 
+    }
+
+    def binary_search(arr: Array[Int], left: Int, right: Int, x: Int): Int =
+    {
+        if(left <= right){
+            val mid = (left + right)/2
+            if(arr[mid] == x){
+                return mid
+            }
+            else if(arr[mid] < x){
+                return binary_search(arr, mid + 1, right, x)
+            }
+            else{
+                return binary_search(arr, left, mid - 1, x)
+            }
+        }
+        else{
+            return -1
+        }
+    }
+}
